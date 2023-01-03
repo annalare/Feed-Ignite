b5a8f7586766d89fa8298f4cfde609fa35c9a248
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2023-01-03 20:35:59 -0300

    feat:header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import styles from "./header.module.css";
+import igniteLogo from "../assets/ignite.png";
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>
+      <img src={igniteLogo} alt="logotipo-ignite" />
     </header>
   );
 }
