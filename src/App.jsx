72de678c71f29fb432dad8674ec0ef61aaa1fcb2
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2023-01-02 23:39:54 -0300

    style:global css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { Header } from "./components/header";
 import { Post } from "./Post";
-import "./styles.css";
-function App() {
-  const [count, setCount] = useState(0);
 
+import "./global.css";
+
+export function App() {
   return (
-    <div className="App">
-      <Post />
-    </div>
+    <>
+      <div>
+        <Header />
+
+        <Post
+          author="Larissa Mendes"
+          content=" Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a "
+        />
+        <Post
+          author="Guilherme Melo"
+          content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's sul"
+        />
+      </div>
+    </>
   );
 }
 
