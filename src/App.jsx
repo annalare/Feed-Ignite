19e0654cb4824631be9294bc636cf02526a6e779
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2023-01-03 21:29:45 -0300

    feat:sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Header } from "./components/header";
 import { Post } from "./Post";
-
+import styles from "./App.module.css";
 import "./global.css";
 
 export function App() {
@@ -9,14 +9,18 @@ export function App() {
       <div>
         <Header />
 
-        <Post
-          author="Larissa Mendes"
-          content=" Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a "
-        />
-        <Post
-          author="Guilherme Melo"
-          content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's sul"
-        />
+        <div className={styles.wrapper}></div>
+        <aside>sidebar</aside>
+        <main>
+          <Post
+            author="Larissa Mendes"
+            content=" Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a "
+          />
+          <Post
+            author="Guilherme Melo"
+            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's sul"
+          />
+        </main>
       </div>
     </>
   );
