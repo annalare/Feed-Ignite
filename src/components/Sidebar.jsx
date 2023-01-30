dc42ca2cf94eff39337f337229bdfea9c113da44
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2023-01-29 22:05:42 -0300

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import styles from "./Sidebar.module.css";
 import folha from "../assets/folha.png";
+import { PencilLine } from "phosphor-react";
 import "../global.css";
 export function Sidebar() {
   return (
@@ -7,11 +8,15 @@ export function Sidebar() {
       <img className={styles.cover} src={folha} alt="" />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/annalare.png" />
         <strong> Larissa Mendes</strong>
         <span>Front End Developer</span>
       </div>
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
